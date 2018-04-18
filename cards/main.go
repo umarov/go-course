a4b83dc5a00649f29f1ec07e557630bacc94d0e8
Muzafar Umarov <umarov@gmail.com>
2018-04-17 22:00:56 -0400

    Initial Commit

---
+++ cards/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cards := []string{newCard(), newCard()}
+	cards = append(cards, "Six of spades")
+
+	for i, card := range cards {
+
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
